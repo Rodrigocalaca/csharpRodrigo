5bec42191cb73335bccd1b6d59d4593103422082
Rodrigo Calaça <rodrigocalacadev@gmail.com>
2022-03-01 23:56:12 -0300

    Add files via upload

---
+++ boletimAluno/boletimAluno/AlunoEnsinoBasico.cs
@@ -0,0 +1,16 @@
+﻿namespace RelatorioAlunos
+{
+    public class AlunoEnsinoBasico : Aluno
+    {
+        public string? Serie { get; set; }
+        public string? Responsavel { get; set; }
+
+        public AlunoEnsinoBasico(string nome, double p1, double p2, string matricula, string serie, string responsavel) : base (nome, p1, p2, matricula)
+        {
+            Serie = serie;
+
+            Responsavel = responsavel;
+        }
+
+    }
+}
