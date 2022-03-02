fa996b20dda7470a9d595112a47fff6489219264
Rodrigo Calaça <rodrigocalacadev@gmail.com>
2022-03-01 23:59:57 -0300

    Add files via upload

---
+++ BoletimAluno/boletimAluno/boletimAluno/Aluno.cs
@@ -0,0 +1,40 @@
+﻿using boletimAluno;
+
+namespace RelatorioAlunos
+{
+    public class Aluno : IAluno
+    {
+        private readonly string _nome;
+        private readonly string _matricula;
+        private readonly double nota1;
+        private readonly double nota2;
+
+
+
+        public Aluno()
+        {
+
+        }
+
+        public Aluno(string nome, double p1, double p2, string matricula)
+        {
+            _nome = nome;
+            this.nota1 = p1;
+            this.nota2 = p2;
+            this._matricula = matricula;
+
+        }
+
+        public string Nome => _nome;
+
+        public string Matricula => _matricula;
+
+        public double PrimeiraProva => nota1;
+
+        public double SegundaProva => nota2;
+
+        public double Media => (nota1 + nota2) / 2;
+
+        public bool Aprovado => Media >= 6;
+    }
+}
---
+++ BoletimAluno/boletimAluno/boletimAluno/AlunoEnsinoSuperior.cs
@@ -0,0 +1,17 @@
+﻿namespace RelatorioAlunos
+{
+    public class AlunoEnsinoSuperior : Aluno
+    {
+        public string? TurnoDasAulas { get; set; }
+
+        public string? Periodo { get; set; }
+
+        public AlunoEnsinoSuperior(string nome, double p1, double p2, string matricula, string turnoDasAulas, string periodo) : base(nome, p1, p2, matricula)
+        {
+            TurnoDasAulas = turnoDasAulas;
+
+            Periodo = periodo;
+
+        }
+    }
+}
